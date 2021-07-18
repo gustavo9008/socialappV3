4228c581076999b883397ce987b0b077c9d3e826
Gustavo Ayala <gustavo.ayala7@gmail.com>
2021-07-18 13:04:00 +0000

    first commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  reactStrictMode: true,
+  images: {
+    domains: ["res.cloudinary.com", "images.unsplash.com"],
+  },
+  env: {
+    mongodburl: "mongodb://10.0.0.60/restful_blog_appv3",
 }
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+// import "../styles/globals.css";
+import React, { Fragment } from "react";
+import NavBar from "../components/layout/NavBar";
+import "../styles/styles.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <NavBar />
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
