4ba2c4da036a0f359c7ca728d8f2bc084664d6b0
Gustavo Ayala <gustavo.ayala7@gmail.com>
2022-03-21 23:59:23 -0700

    comment out editor code

--- components/posts/editor.jsx
+++ components/posts/editor.jsx
@@ -1,58 +1,58 @@
-import React from "react";
-import { EditorState } from "draft-js";
-import dynamic from "next/dynamic";
-import "draft-js/dist/Draft.css";
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-const Editor = dynamic(
-  () => import("react-draft-wysiwyg").then((mod) => mod.Editor),
-  { ssr: false }
-);
+// import React from "react";
+// import { EditorState } from "draft-js";
+// import dynamic from "next/dynamic";
+// import "draft-js/dist/Draft.css";
+// import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+// const Editor = dynamic(
+//   () => import("react-draft-wysiwyg").then((mod) => mod.Editor),
+//   { ssr: false }
+// );
 
-export default function MyEditor() {
-  const [editorState, setEditorState] = React.useState(() =>
-    EditorState.createEmpty()
-  );
+// export default function MyEditor() {
+//   const [editorState, setEditorState] = React.useState(() =>
+//     EditorState.createEmpty()
+//   );
 
-  const editor = React.useRef(null);
-  function focusEditor() {
-    editor.current.focus();
-  }
+//   const editor = React.useRef(null);
+//   function focusEditor() {
+//     editor.current.focus();
+//   }
 
-  return (
-    <div
-      style={{ border: "1px solid black", minHeight: "6em", cursor: "text" }}
-      onClick={focusEditor}
-    >
-      <Editor
-        editorState={editorState}
-        toolbarClassName="toolbar-class"
-        wrapperClassName="wrapper-class"
-        editorClassName="editor-class"
-        ref={editor}
-        onChange={setEditorState}
-        placeholder="Write something!"
-        toolbar={{
-          options: [
-            "inline",
-            "blockType",
-            "fontSize",
-            "fontFamily",
-            "list",
-            "textAlign",
-            "colorPicker",
-            "link",
-            "embedded",
-            "emoji",
-            "image",
-            "history",
-          ],
-          inline: { inDropdown: true },
-          list: { inDropdown: true },
-          textAlign: { inDropdown: true },
-          link: { inDropdown: true },
-          history: { inDropdown: true },
-        }}
-      />
-    </div>
-  );
-}
+//   return (
+//     <div
+//       style={{ border: "1px solid black", minHeight: "6em", cursor: "text" }}
+//       onClick={focusEditor}
+//     >
+//       <Editor
+//         editorState={editorState}
+//         toolbarClassName="toolbar-class"
+//         wrapperClassName="wrapper-class"
+//         editorClassName="editor-class"
+//         ref={editor}
+//         onChange={setEditorState}
+//         placeholder="Write something!"
+//         toolbar={{
+//           options: [
+//             "inline",
+//             "blockType",
+//             "fontSize",
+//             "fontFamily",
+//             "list",
+//             "textAlign",
+//             "colorPicker",
+//             "link",
+//             "embedded",
+//             "emoji",
+//             "image",
+//             "history",
+//           ],
+//           inline: { inDropdown: true },
+//           list: { inDropdown: true },
+//           textAlign: { inDropdown: true },
+//           link: { inDropdown: true },
+//           history: { inDropdown: true },
+//         }}
+//       />
+//     </div>
+//   );
+// }
