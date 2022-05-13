8bb0f70014894220accd2afec107100c212ed3c5
Gustavo Ayala <gustavo.ayala7@gmail.com>
2022-05-13 00:17:58 -0700

    next auth ref test

--- next.config.js
+++ next.config.js
@@ -3,4 +3,30 @@ module.exports = {
   images: {
     domains: ["res.cloudinary.com", "images.unsplash.com"],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: "camera=(); battery=(self); geolocation=(); microphone=('https://somewhere.com')",
+          },
+        ],
+      },
+    ];
+  },
 };
